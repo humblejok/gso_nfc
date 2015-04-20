a03add4e7443d48b2da25d73abb79c73e7ff27b3
De Jonckheere Stéphane <humble.jok@gmail.com>
2015-04-20 10:57:24 +0200

    Working

--- src/org/gso/network/messaging/gui/AccountSetupWindow.java
+++ src/org/gso/network/messaging/gui/AccountSetupWindow.java
@@ -1,23 +1,38 @@
 package org.gso.network.messaging.gui;
 
-import java.awt.GridLayout;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 
 import org.gso.network.messaging.gui.resources.ResourcesProvider;
 
 public class AccountSetupWindow extends JFrame implements WindowListener {
 	
+	protected JTextField login = new JTextField("", 16);
+	protected JTextField password = new JPasswordField("", 16);
+	protected JPanel mainPanel = new JPanel(new MigLayout("wrap 3"));
+	
+	
 	public AccountSetupWindow() {
 		super("Accounts setup");
 		this.setSize(300, 500);
 		this.setAlwaysOnTop(true);
-		this.setLayout(new GridLayout(1,1));
 		this.addWindowListener(this);
 		this.setIconImage(ResourcesProvider.getInstance().getImage("bubble.png"));
 		
+		mainPanel.add(new JLabel("Login:"));
+		mainPanel.add(login, "span 2");
+		mainPanel.add(new JLabel("Password:"));
+		mainPanel.add(password, "span 2");
+		
+		this.add(mainPanel);
 	}
 	
 	@Override
